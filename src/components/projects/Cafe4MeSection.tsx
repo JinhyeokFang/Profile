e952adbcd579e56112f546c566810afa312aac97
jinhyeokfang <jinhyeokfang@gmail.com>
2023-04-16 23:05:20 +0900

    feat: more project description

--- src/components/projects/Cafe4MeSection.tsx
+++ src/components/projects/Cafe4MeSection.tsx
@@ -31,20 +31,46 @@ export const Cafe4MeSection = () => {
               </header>
               <article className={styles.article}>
                 <div>
-                    <h2 className={styles.subtitle}>프로젝트 의의</h2>
-                    <p className={styles.description}>AWS로 웹서비스를 처음으로 배포</p>
-                    <p className={styles.description}>Github Actions로 CD(Continuous Deployment) 사용</p>
-                    <p className={styles.description}>Docker로 클라이언트와 서버를 하나의 컨테이너로 묶어 배포</p>
-                    <p className={styles.description}>첫 Next.js 클라이언트 개발</p>
-                </div>
-                <hr className='lighter' />
-                <div>
                     <h2 className={styles.subtitle}>주요 기능</h2>
                     <p className={styles.description}>지도에서 위치로 카페 검색</p>
                     <p className={styles.description}>카페 추가</p>
                     <p className={styles.description}>리뷰 추가 / 삭제</p>
                     <p className={styles.description}>주소로 카페 검색</p>
                     <p className={styles.description}>이름으로 카페 검색</p>
+                </div>
+                <hr className='lighter' />
+                <div>
+                    <h2 className={styles.subtitle}>프로젝트 의의</h2>
+                    <ol className={styles.projectDescription}>
+                      <ul>
+                        <h3 className={styles.description}>1. AWS로 웹서비스를 처음으로 배포</h3>
+                        <Image className={styles.image} src={projectImage} alt="" width="500" height="500" />
+                        <p>
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nemo libero error quasi dolorem excepturi quo distinctio iusto quia, vero, sint accusamus explicabo veritatis itaque perspiciatis aliquam cupiditate. Itaque, deserunt.
+                        </p>
+                      </ul>
+                      <ul>
+                        <h3 className={styles.description}>2. Github Actions로 CD(Continuous Deployment) 사용</h3>
+                        <Image className={styles.image} src={projectImage} alt="" width="500" height="500" />
+                        <p>
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nemo libero error quasi dolorem excepturi quo distinctio iusto quia, vero, sint accusamus explicabo veritatis itaque perspiciatis aliquam cupiditate. Itaque, deserunt.
+                        </p>
+                      </ul>
+                      <ul>
+                        <h3 className={styles.description}>3. Docker로 클라이언트와 서버를 하나의 컨테이너로 묶어 배포</h3>
+                        <Image className={styles.image} src={projectImage} alt="" width="500" height="500" />
+                        <p>
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nemo libero error quasi dolorem excepturi quo distinctio iusto quia, vero, sint accusamus explicabo veritatis itaque perspiciatis aliquam cupiditate. Itaque, deserunt.
+                        </p>
+                      </ul>
+                      <ul>
+                        <h3 className={styles.description}>4. 첫 Next.js 클라이언트 개발</h3>
+                        <Image className={styles.image} src={projectImage} alt="" width="500" height="500" />
+                        <p>
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nemo libero error quasi dolorem excepturi quo distinctio iusto quia, vero, sint accusamus explicabo veritatis itaque perspiciatis aliquam cupiditate. Itaque, deserunt.
+                        </p>
+                      </ul>
+                    </ol>
                 </div>
                 <hr className='lighter' />
                 <div>
@@ -58,8 +84,6 @@ export const Cafe4MeSection = () => {
                 <div>
                     <h2 className={styles.subtitle}>느낀점</h2>
                     <p className={styles.description}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis illo voluptates beatae, eaque dignissimos aliquam magni ducimus ut quo praesentium, eius similique sint quidem veritatis, blanditiis maiores! Vitae, nobis culpa.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis illo voluptates beatae, eaque dignissimos aliquam magni ducimus ut quo praesentium, eius similique sint quidem veritatis, blanditiis maiores! Vitae, nobis culpa.
                       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis illo voluptates beatae, eaque dignissimos aliquam magni ducimus ut quo praesentium, eius similique sint quidem veritatis, blanditiis maiores! Vitae, nobis culpa.
                       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis illo voluptates beatae, eaque dignissimos aliquam magni ducimus ut quo praesentium, eius similique sint quidem veritatis, blanditiis maiores! Vitae, nobis culpa.
                       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis illo voluptates beatae, eaque dignissimos aliquam magni ducimus ut quo praesentium, eius similique sint quidem veritatis, blanditiis maiores! Vitae, nobis culpa.
--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,10 +1,17 @@
+import { useRouter } from 'next/router'
 import styles from '../styles/title.module.css'
 
 export const Title = () => {
+  const router = useRouter();
+
+  const toMain = () => {
+    router.push('/');
+  }
+
     return (
         <>
-            <header className={styles.wrapper}>
-                <h1 className={styles.title}>Jin-hyeok Bang</h1>
+            <header className={styles.wrapper} onClick={toMain}>
+                <h1 className={styles.title}>개발자 방진혁</h1>
                 <hr className={styles.bottomLine}/>
             </header>
         </>
