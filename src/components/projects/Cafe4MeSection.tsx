ed77cb156469d6ad56d64bbe57de521e660b45ca
jinhyeokfang <jinhyeokfang@gmail.com>
2023-04-29 22:15:38 +0900

    design

--- src/components/projects/Cafe4MeSection.tsx
+++ src/components/projects/Cafe4MeSection.tsx
@@ -41,44 +41,44 @@ export const Cafe4MeSection = () => {
                 <hr className='lighter' />
                 <div>
                     <h2 className={styles.subtitle}>프로젝트 의의</h2>
-                    <ol className={styles.projectDescription}>
-                      <ul>
+                    <ol className={styles.significanceList}>
+                      <li className={styles.significance}>
                         <h3 className={styles.description}>1. AWS로 웹서비스를 처음으로 배포</h3>
-                        <Image className={styles.image} src={projectImage} alt="" width="500" height="500" />
-                        <p>
-                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nemo libero error quasi dolorem excepturi quo distinctio iusto quia, vero, sint accusamus explicabo veritatis itaque perspiciatis aliquam cupiditate. Itaque, deserunt.
-                        </p>
-                      </ul>
-                      <ul>
+                        <div>
+                          <Image className={styles.image} src={projectImage} alt="" width="400" height="400" />
+                          <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nemo libero error quasi dolorem excepturi quo distinctio iusto quia, vero, sint accusamus explicabo veritatis itaque perspiciatis aliquam cupiditate. Itaque, deserunt.
+                          </p>
+                        </div>
+                      </li>
+                      <li className={styles.significance}>
                         <h3 className={styles.description}>2. Github Actions로 CD(Continuous Deployment) 사용</h3>
-                        <Image className={styles.image} src={projectImage} alt="" width="500" height="500" />
-                        <p>
-                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nemo libero error quasi dolorem excepturi quo distinctio iusto quia, vero, sint accusamus explicabo veritatis itaque perspiciatis aliquam cupiditate. Itaque, deserunt.
-                        </p>
-                      </ul>
-                      <ul>
+                        <div>
+                          <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nemo libero error quasi dolorem excepturi quo distinctio iusto quia, vero, sint accusamus explicabo veritatis itaque perspiciatis aliquam cupiditate. Itaque, deserunt.
+                          </p>
+                          <Image className={styles.image} src={projectImage} alt="" width="400" height="400" />
+                        </div>
+                      </li>
+                      <li className={styles.significance}>
                         <h3 className={styles.description}>3. Docker로 클라이언트와 서버를 하나의 컨테이너로 묶어 배포</h3>
-                        <Image className={styles.image} src={projectImage} alt="" width="500" height="500" />
-                        <p>
-                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nemo libero error quasi dolorem excepturi quo distinctio iusto quia, vero, sint accusamus explicabo veritatis itaque perspiciatis aliquam cupiditate. Itaque, deserunt.
-                        </p>
-                      </ul>
-                      <ul>
+                        <div>
+                          <Image className={styles.image} src={projectImage} alt="" width="400" height="400" />
+                          <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nemo libero error quasi dolorem excepturi quo distinctio iusto quia, vero, sint accusamus explicabo veritatis itaque perspiciatis aliquam cupiditate. Itaque, deserunt.
+                          </p>
+                        </div>
+                      </li>
+                      <li className={styles.significance}>
                         <h3 className={styles.description}>4. 첫 Next.js 클라이언트 개발</h3>
-                        <Image className={styles.image} src={projectImage} alt="" width="500" height="500" />
-                        <p>
-                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nemo libero error quasi dolorem excepturi quo distinctio iusto quia, vero, sint accusamus explicabo veritatis itaque perspiciatis aliquam cupiditate. Itaque, deserunt.
-                        </p>
-                      </ul>
+                        <div>
+                          <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nemo libero error quasi dolorem excepturi quo distinctio iusto quia, vero, sint accusamus explicabo veritatis itaque perspiciatis aliquam cupiditate. Itaque, deserunt.
+                          </p>
+                          <Image className={styles.image} src={projectImage} alt="" width="400" height="400" />
+                        </div>
+                      </li>
                     </ol>
-                </div>
-                <hr className='lighter' />
-                <div>
-                    <h2 className={styles.subtitle}>개선이 필요한 점</h2>
-                    <p className={styles.description}>테스트 코드 추가</p>
-                    <p className={styles.description}>AWS 기반 로드벨런싱 추가</p>
-                    <p className={styles.description}>클라이언트 SSR로 전환</p>
-                    <p className={styles.description}>카페 데이터 추가</p>
                 </div>
                 <hr className='lighter' />
                 <div>
@@ -94,9 +94,23 @@ export const Cafe4MeSection = () => {
                 </div>
                 <hr className='lighter' />
                 <div>
+                    <h2 className={styles.subtitle}>개선이 필요한 점</h2>
+                    <p className={styles.description}>테스트 코드 추가</p>
+                    <p className={styles.description}>AWS 기반 로드벨런싱 추가</p>
+                    <p className={styles.description}>클라이언트 SSR로 전환</p>
+                    <p className={styles.description}>카페 데이터 추가</p>
+                </div>
+                <hr className='lighter' />
+                <div>
                     <h2 className={styles.subtitle}>기타</h2>
-                    <p className={styles.description}>기존에 사용하던 서버 레포지토리 주소: https://github.com/JinhyeokFang/cafe-for-me-server</p>
-                    <p className={styles.description}>기존에 사용하던 클라이언트 레포지토리 주소: https://github.com/JinhyeokFang/cafe-for-me-client</p>
+                    <p className={styles.description}>
+                      기존에 사용하던 서버 레포지토리 주소: &nbsp;
+                      <a href="https://github.com/JinhyeokFang/cafe-for-me-server">https://github.com/JinhyeokFang/cafe-for-me-server</a>
+                    </p>
+                    <p className={styles.description}>
+                      기존에 사용하던 클라이언트 레포지토리 주소: &nbsp;
+                      <a href="https://github.com/JinhyeokFang/cafe-for-me-client">https://github.com/JinhyeokFang/cafe-for-me-client</a>
+                    </p>
                 </div>
               </article>
             </div>
