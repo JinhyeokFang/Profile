70da1f9803f087f2fa8decf5f3630a6602bf1232
jinhyeokfang <jinhyeokfang@gmail.com>
2023-06-24 00:46:11 +0900

    update style

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -13,17 +13,17 @@ export const Profile = () => {
                     <div>
                         <h1 className={styles.name}><span>개발자 방진혁 | </span><span className={styles.catchword}>성장하는 코드를 지향합니다 :)</span></h1>
                         <p className={styles.description}>
-                            미래를 예측하기보단 현재에 집중하고자 노력합니다. <br/>
-                            확장성을 고려하는 코드를 지향합니다.
+                            미래를 예측하기보단 <b>현재에</b> 집중하고자 노력합니다. <br/>
+                            <b>확장성</b>을 고려하는 코드를 지향합니다.
                         </p>
                     </div>
                     <div>
-                        <p className={styles.link}>Github: <a href='https://github.com/JinhyeokFang'>https://github.com/JinhyeokFang</a></p>
-                        <p className={styles.link}>E-Mail: hyeki0206@naver.com, jinhyeokfang@gmail.com</p>
+                        <p className={styles.link}><b>Github</b>: <a href='https://github.com/JinhyeokFang'>https://github.com/JinhyeokFang</a></p>
+                        <p className={styles.link}><b>E-Mail</b>: hyeki0206@naver.com, jinhyeokfang@gmail.com</p>
                     </div>
                     <div>
-                        <p className={styles.school}>경기대학교 AI컴퓨터공학부 컴퓨터공학전공 <span className={styles.studyPeriod}>(21.03 ~ )</span></p>
-                        <p className={styles.school}>선린인터넷고등학교 소프트웨어과 졸업 <span className={styles.studyPeriod}>(18.03 ~ 21.02)</span></p>
+                        <p className={styles.school}><b>경기대학교 AI컴퓨터공학부 컴퓨터공학전공</b> 재학 <span className={styles.studyPeriod}>(21.03 ~ )</span></p>
+                        <p className={styles.school}><b>선린인터넷고등학교 소프트웨어과</b> 졸업 <span className={styles.studyPeriod}>(18.03 ~ 21.02)</span></p>
                     </div>
                 </div>
             </div>
