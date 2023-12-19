a6a54f53d1b31ede388b8645434c2d05b21fd356
JinhyeokBang <jinhyeokfang@gmail.com>
2023-12-19 13:56:01 +0900

    update

--- src/components/profile/profile-information.tsx
+++ src/components/profile/profile-information.tsx
@@ -8,8 +8,8 @@ export const ProfileInformation = () => {
               <p className={styles.link}><b>Mail</b>: hyeki0206@naver.com, jinhyeokfang@gmail.com</p>
           </section>
           <section>
-              <p className={styles.school}><b>경기대학교 AI컴퓨터공학부 컴퓨터공학전공</b> 재학 <span className={styles.studyPeriod}>(21.03 ~ )</span></p>
-              <p className={styles.school}><b>선린인터넷고등학교 소프트웨어과</b> 졸업 <span className={styles.studyPeriod}>(18.03 ~ 21.02)</span></p>
+              <p className={styles.school}><b>경기대 컴퓨터공학전공</b> 재학 <span className={styles.studyPeriod}>(21.03 ~ )</span></p>
+              <p className={styles.school}><b>선린인터넷고 소프트웨어과</b> 졸업 <span className={styles.studyPeriod}>(18.03 ~ 21.02)</span></p>
           </section>
           <section>
               <p className={styles.school}>정보처리기능사 | SQL 개발자</p>
