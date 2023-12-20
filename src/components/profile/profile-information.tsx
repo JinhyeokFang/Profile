c1057b4c91e71c57eeae325f67b908fc953edce4
JinhyeokBang <jinhyeokfang@gmail.com>
2023-12-20 11:30:48 +0900

    update

--- src/components/profile/profile-information.tsx
+++ src/components/profile/profile-information.tsx
@@ -4,12 +4,12 @@ export const ProfileInformation = () => {
     return (
         <>
           <section>
-              <p className={styles.link}><b>Github</b>: <a href='https://github.com/JinhyeokFang'>https://github.com/JinhyeokFang</a></p>
-              <p className={styles.link}><b>Mail</b>: hyeki0206@naver.com, jinhyeokfang@gmail.com</p>
+              <p className={styles.link}><a href='https://github.com/JinhyeokFang'>https://github.com/JinhyeokFang</a></p>
+              <p className={styles.link}>hyeki0206@naver.com, jinhyeokfang@gmail.com</p>
           </section>
           <section>
-              <p className={styles.school}><b>경기대 컴퓨터공학전공</b> 재학 <span className={styles.studyPeriod}>(21.03 ~ )</span></p>
-              <p className={styles.school}><b>선린인터넷고 소프트웨어과</b> 졸업 <span className={styles.studyPeriod}>(18.03 ~ 21.02)</span></p>
+              <p className={styles.school}><b>경기대</b> 컴퓨터공학전공 재학 <span className={styles.studyPeriod}>(21.03 ~ )</span></p>
+              <p className={styles.school}><b>선린인터넷고</b> 소프트웨어과 졸업 <span className={styles.studyPeriod}>(18.03 ~ 21.02)</span></p>
           </section>
           <section>
               <p className={styles.school}>정보처리기능사 | SQL 개발자</p>
