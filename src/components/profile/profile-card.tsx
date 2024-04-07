45cf814d616c1a9e69c05ee365c0e0ada43d34b0
방진혁 <jinhyeokfang@gmail.com>
2024-04-07 14:35:07 +0900

    최근 생각 반영하기

--- src/components/profile/profile-card.tsx
+++ src/components/profile/profile-card.tsx
@@ -10,10 +10,9 @@ export const ProfileCard = () => {
         </section>
         <section className={styles.infoSection}>
             <div>
-                <h1 className={styles.name}><span>도전하는 백엔드 방진혁</span></h1>
+                <h1 className={styles.name}><span>적정기술 개발자 방진혁</span></h1>
                 <p className={styles.description}>
                     사용자를 위한 서비스에 <b>몰입</b>하는 개발자<br/>
-                    <b>열정</b>가득한 도전하는 백엔드<br/>
                 </p>
             </div>
         </section>
