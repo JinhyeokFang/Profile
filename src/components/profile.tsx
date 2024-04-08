7d9e5531f328846ead24ad4b601250e9dea0d70d
방진혁 <jinhyeokfang@gmail.com>
2024-04-08 10:09:07 +0900

    Update profile.tsx

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,7 +1,6 @@
 import styles from '../styles/profile.module.css'
 import { ProfileCard } from './profile/profile-card'
 import { ProfileInformation } from './profile/profile-information'
-import { TechStack } from './profile/tech-stack'
 
 export const Profile = () => {
     return (
@@ -10,8 +9,6 @@ export const Profile = () => {
             <ProfileCard />
             <h1 className={styles.title}>프로필</h1>
             <ProfileInformation />
-            <h1 className={styles.title}>기술 스택</h1>
-            <TechStack />
             <h1 className={styles.title}>포트폴리오 (Notion)</h1>
             <a href="https://jinhy.uk/portfolio">https://jinhy.uk/portfolio</a>
           </div>
