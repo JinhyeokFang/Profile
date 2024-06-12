6b4a86c61c23b2a020708b48d101a8fdd05089d9
방진혁 <jinhyeokfang@gmail.com>
2024-06-12 17:24:57 +0900

    Update profile-information.tsx

--- src/components/profile/profile-information.tsx
+++ src/components/profile/profile-information.tsx
@@ -8,12 +8,12 @@ export const ProfileInformation = () => {
               <p className={styles.link}>hyeki0206@naver.com</p>
           </section>
           <section>
-              <p className={styles.school}><b>경기대</b> 컴퓨터공학전공 재학</p>
-              <p className={styles.school}><b>선린인터넷고</b> 소프트웨어과 졸업</p>
+              <p className={styles.school}>소마 15기 | UMC 5기</p>
+              <p className={styles.school}>SQL 개발자 | 정보처리기능사</p>
           </section>
           <section>
-              <p className={styles.school}>SQL 개발자 | 정보처리기능사</p>
-              <p className={styles.school}>5th UMC | 20th IWOP</p>
+              <p className={styles.school}><b>경기대</b> 컴퓨터공학전공 재학</p>
+              <p className={styles.school}><b>선린인터넷고</b> 소프트웨어과 졸업</p>
           </section>
         </>
     )
